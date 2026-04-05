9d4dc14cd8462398d8df37e2d01aaf1b97415302
ericj <eric.johnson@student.nmt.edu>
2026-04-05 16:34:48 -0600

    Added creation of patients and provider demonstration

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,20 @@ import java.util.List;
 // Main method to test functionality of basic class code translations
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Checkpoint 1: Demo Appointment Scheduling");
+        System.out.println("Checkpoint 1.5: Demo Appointment Scheduling");
 
-        // Initialize list for patients and providers
-        List<Patient> patients = new ArrayList<>();
-        List<Provider> providers = new ArrayList<>();
-        // Initialize AppointmentManager
         AppointmentManager manager = new AppointmentManager();
 
-        // Create patient
-        Patient patient1 = new Patient(101, "John Doe", LocalDate.of(2000,01,01),"johndoe@example.com");
-        // Add patient1 to list and print patient1 details
-        patients.add(patient1);
-        System.out.println("Registered Patient: " + patient1.toString());
-
-        // Create provider
+        System.out.println("TEST 1: Valid entry creation");
+        // Create patients (2) and provider objects
+        Patient patient1 = new Patient(101, "John Doe", LocalDate.of(2000,01,01), "johndoe@example.com");
+        Patient patient2 = new Patient(102, "Jane Doe", LocalDate.of(1999,12,31), "jane.doe@example.com");
         Provider provider1 = new Provider(701, "Dr. Smith", "Cardiology", "Room A-11");
-        providers.add(provider1);
-        // Add provider1 to list and print provider1 details
-        System.out.println("Registered Provider: " + provider1.toString());
 
-        // Schedule appointment
-        System.out.println("Appointment schedule demo:");
-        // Schedule appointment on: April 1, 2026: 9AM to 9:30 AM
-        // start and end times will be handled by the AppointmentManager
-        LocalDateTime start = LocalDateTime.of(2026, 4, 1, 9, 0);
-        LocalDateTime end = LocalDateTime.of(2026, 4, 1, 9, 30);
-
-        // Create Appointment object, utilize AppointmentManager schedule method
-        Appointment newAppointment = manager.scheduleAppointment(patient1, provider1, "Checkup", start, end);
-
-        if (newAppointment != null) {
-            System.out.println("New Appointment successfully scheduled for " + newAppointment.getPatient().getName() + " with provider " + newAppointment.getProvider().getName() + " at " + newAppointment.getStartDateTime());
-        } else {
-            System.out.println("Failed to create new appointment (Implementation TBA, scheduleAppointment() returned null)");
-        }
+        // Add patients and provider to AppointmentManager map
+        manager.addPatient(patient1);
+        manager.addPatient(patient2);
+        manager.addProvider(provider1);
+        System.out.println("Successfully added (2) patients and provider to the system.\n");
     }
 }
