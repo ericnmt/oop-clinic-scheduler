9adfb9b7638461c3e65c9ae0f0e947e73299d6ab
ericj <eric.johnson@student.nmt.edu>
2026-04-15 13:46:01 -0600

    Revise Javadoc comments for Patient class.

--- src/Patient.java
+++ src/Patient.java
@@ -1,17 +1,30 @@
 import java.time.LocalDate;
 
+/**
+ * Class to represent a person/patient that requests to schedule an Appointment.
+ */
 public class Patient{
+    /**
+     * The unique ID of a patient.
+     */
     private int patientId;
+    /**
+     * The name of the patient.
+     */
     private String name;
+    /**
+     * The patient's date of birth
+     */
     private LocalDate dateOfBirth;
     private String contactInfo;
 
     /**
-     * Constructor, all parameters must exist
-     * @param patientId of Patient
-     * @param name of Patient
-     * @param dateOfBirth of Patient
-     * @param contactInfo of Patient
+     * Create a new patient in the system.
+     *
+     * @param patientId of the Patient
+     * @param name of the Patient
+     * @param dateOfBirth of the Patient
+     * @param contactInfo of the Patient
      */
     public Patient(int patientId, String name, LocalDate dateOfBirth, String contactInfo) {
         this.patientId = patientId;
@@ -21,41 +34,87 @@ public class Patient{
     }
 
     // Getters and Setters
+    /**
+     * Get the ID of a patient.
+     *
+     * @return the patient's ID
+     */
     public int getPatientId() {
         return patientId;
     }
 
+    /**
+     * Set the ID of a patient.
+     * patientId should only be set ONCE when the patient is added to the system.
+     *
+     * @param patientId of the patient
+     */
     // REVISE: A patientId should always remain the same
     public void setPatientId(int patientId) {
         this.patientId = patientId;
     }
 
+    /**
+     * Get the name of a patient.
+     *
+     * @return the patient's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the name of a patient.
+     *
+     * @param name of the patient
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the date of birth of a patient.
+     *
+     * @return the patient's date of birth
+     */
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
+    /**
+     * Set the date of birth of a patient.
+     *
+     * @param dateOfBirth of the patient
+     */
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * Get the contact information of a patient.
+     * More than one method of contact may exist (TBA).
+     *
+     * @return patient's contact information
+     */
     public String getContactInfo() {
         return contactInfo;
     }
 
+    /**
+     * Set the contact information for a patient.
+     *
+     * @param contactInfo of the patient
+     */
     // REVISE: A patient may have multiple methods of contact
     public void setContactInfo(String contactInfo) {
         this.contactInfo = contactInfo;
     }
 
-    // Patient toString method
+    /**
+     * Compose a patient's information into a readable string format.
+     *
+     * @return patient's information
+     */
     @Override
     public String toString() {
         return "Patient{" +
