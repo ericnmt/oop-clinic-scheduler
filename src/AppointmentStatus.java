8f80088cb35fa9c02eef032e8f3e29320500d02f
ericj <eric.johnson@student.nmt.edu>
2026-04-15 13:45:46 -0600

    Add Javadoc comments to AppointmentStatus class.

--- src/AppointmentStatus.java
+++ src/AppointmentStatus.java
@@ -1,5 +1,17 @@
+/**
+ * ENUMERATION class to represent the three status conditions an Appointment can have.
+ */
 public enum AppointmentStatus {
+    /**
+     * An appointment is scheduled.
+     */
     SCHEDULED,
+    /**
+     * An appointment is completed.
+     */
     COMPLETED,
+    /**
+     * An appointment is canceled.
+     */
     CANCELLED
 }
