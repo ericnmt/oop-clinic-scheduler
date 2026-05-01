867b7e7b619bdfd013bf68d9b58c69af5b392238
ericj <eric.johnson@student.nmt.edu>
2026-05-01 12:55:50 -0600

    cleaned up formatting for schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,28 @@
 -- Patient Table
 CREATE TABLE IF NOT EXISTS Patient (
-                                       PatientID INTEGER PRIMARY KEY,
-                                       Name TEXT NOT NULL,
-                                       DateOfBirth TEXT NOT NULL,
-                                       ContactInfo TEXT
+    PatientID INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    DateOfBirth TEXT NOT NULL,
+    ContactInfo TEXT
 );
 
 -- Provider Table
 CREATE TABLE IF NOT EXISTS Provider (
-                                        ProviderID INTEGER PRIMARY KEY,
-                                        Name TEXT NOT NULL,
-                                        Specialty TEXT NOT NULL,
-                                        Location TEXT NOT NULL
+    ProviderID INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    Specialty TEXT NOT NULL,
+    Location TEXT NOT NULL
 );
 
 -- Appointment Table
 CREATE TABLE IF NOT EXISTS Appointment (
-                                           AppointmentID INTEGER PRIMARY KEY AUTOINCREMENT,
-                                           StartTime TEXT NOT NULL,
-                                           EndTime TEXT NOT NULL,
-                                           Status TEXT NOT NULL,
-                                           Reason TEXT NOT NULL,
-                                           PatientID INTEGER NOT NULL,
-                                           ProviderID INTEGER NOT NULL,
-                                           FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
+    AppointmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+    StartTime TEXT NOT NULL,
+    EndTime TEXT NOT NULL,
+    Status TEXT NOT NULL,
+    Reason TEXT NOT NULL,
+    PatientID INTEGER NOT NULL,
+    ProviderID INTEGER NOT NULL,
+    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
     FOREIGN KEY (ProviderID) REFERENCES Provider(ProviderID)
     );
