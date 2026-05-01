6ea4904ec63f688d62675d3154c5c7889bbab0ea
AbimaelLozanoNMT <abimael.lozano@student.nmt.edu>
2026-05-01 12:03:24 -0600

    Initialized CLI structure Set up a Scanner-while loop in SchedulerApplication.java Designed the main UI Implemented input validation for handling errors Ensured that business logic from appointmentManager was applied Ran full app and test functionality (Please verify) Ensured data persists after shutting down Spring Boot by updating schema.sql Removed Main.java

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,32 +1,28 @@
-DROP TABLE IF EXISTS Appointment;
-DROP TABLE IF EXISTS Patient;
-DROP TABLE IF EXISTS Provider;
-
 -- Patient Table
-CREATE TABLE Patient (
-    PatientID INTEGER PRIMARY KEY,
-    Name TEXT NOT NULL,
-    DateOfBirth TEXT NOT NULL,
-    ContactInfo TEXT
+CREATE TABLE IF NOT EXISTS Patient (
+                                       PatientID INTEGER PRIMARY KEY,
+                                       Name TEXT NOT NULL,
+                                       DateOfBirth TEXT NOT NULL,
+                                       ContactInfo TEXT
 );
 
 -- Provider Table
-CREATE TABLE Provider (
-    ProviderID INTEGER PRIMARY KEY,
-    Name TEXT NOT NULL,
-    Specialty TEXT NOT NULL,
-    Location TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS Provider (
+                                        ProviderID INTEGER PRIMARY KEY,
+                                        Name TEXT NOT NULL,
+                                        Specialty TEXT NOT NULL,
+                                        Location TEXT NOT NULL
 );
 
 -- Appointment Table
-CREATE TABLE Appointment (
-    AppointmentID INTEGER PRIMARY KEY AUTOINCREMENT,
-    StartTime TEXT NOT NULL,
-    EndTime TEXT NOT NULL,
-    Status TEXT NOT NULL,
-    Reason TEXT NOT NULL,
-    PatientID INTEGER NOT NULL,
-    ProviderID INTEGER NOT NULL,
-    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
+CREATE TABLE IF NOT EXISTS Appointment (
+                                           AppointmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                           StartTime TEXT NOT NULL,
+                                           EndTime TEXT NOT NULL,
+                                           Status TEXT NOT NULL,
+                                           Reason TEXT NOT NULL,
+                                           PatientID INTEGER NOT NULL,
+                                           ProviderID INTEGER NOT NULL,
+                                           FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
     FOREIGN KEY (ProviderID) REFERENCES Provider(ProviderID)
-);
+    );
