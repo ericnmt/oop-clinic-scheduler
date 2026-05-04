e0c27f7ef33635b4b009a764872a754e3b547982
ericj <eric.johnson@student.nmt.edu>
2026-05-04 14:49:25 -0600

    Removed ON CASCADE constraint for foreign key reference in Appointment table, added IsActive flags to Patient and Provider tables.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS Patient (
     PatientID INTEGER PRIMARY KEY,
     Name TEXT NOT NULL,
     DateOfBirth TEXT NOT NULL,
-    ContactInfo TEXT NOT NULL
+    ContactInfo TEXT NOT NULL,
+    IsActive INTEGER DEFAULT 1
 );
 
 -- Provider Table
@@ -11,7 +12,8 @@ CREATE TABLE IF NOT EXISTS Provider (
     ProviderID INTEGER PRIMARY KEY,
     Name TEXT NOT NULL,
     Specialty TEXT NOT NULL,
-    Location TEXT NOT NULL
+    Location TEXT NOT NULL,
+    IsActive INTEGER DEFAULT 1
 );
 
 -- Appointment Table
@@ -23,6 +25,6 @@ CREATE TABLE IF NOT EXISTS Appointment (
     Reason TEXT NOT NULL,
     PatientID INTEGER NOT NULL,
     ProviderID INTEGER NOT NULL,
-    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID) ON DELETE CASCADE,
-    FOREIGN KEY (ProviderID) REFERENCES Provider(ProviderID) ON DELETE CASCADE
+    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
+    FOREIGN KEY (ProviderID) REFERENCES Provider(ProviderID)
 );
